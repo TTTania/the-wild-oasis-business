f47d18a4cfc9d00a2d4a65f5df447ba78a55e9e1
tania wang <tania.y.wang@gmail.com>
2024-05-24 10:53:27 +1000

    Modal window reuse

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -17,28 +17,3 @@ function AddCabin() {
   );
 }
 export default AddCabin;
-
-//{
-/* <Modal.Open opens="table">
-        <Button>Add new cabin</Button>
-      </Modal.Open>
-      <Modal.Window name="table">
-        <CreateCabinForm />
-      </Modal.Window> */
-//}
-// function AddCabin() {
-//   const [isOpenModel, setIsOpenModel] = useState(false);
-
-//   return (
-//     <div>
-//       <Button onClick={() => setIsOpenModel((show) => !show)}>
-//         Add new cabin
-//       </Button>
-//       {isOpenModel && (
-//         <Modal onClose={() => setIsOpenModel(false)}>
-//           <CreateCabinForm onCloseModal={() => setIsOpenModel(false)} />
-//         </Modal>
-//       )}
-//     </div>
-//   );
-// }
