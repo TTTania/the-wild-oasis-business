80d98bfb1df91a6ba0d5c8e47eed0333d9a0f153
tania wang <tania.y.wang@gmail.com>
2024-05-26 21:50:10 +1000

    Sort feature added to Bookings page

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -9,7 +9,13 @@ export async function getBookings({ filter, sortBy }) {
     );
 
   //FIlter
-  if (filter !== null) query = query.[filter.method || 'eq'](filter.field, filter.value);
+  if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
+
+  //Sort
+  if (sortBy)
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
 
   const { data, error } = await query;
 
